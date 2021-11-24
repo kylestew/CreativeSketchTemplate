cc0f1786acbfff0bd5dac646520178e7d32a0b2e
Kyle Stewart <kylestew@gmail.com>
2021-11-23 20:39:07 -0700

    ...

--- sketch.js
+++ sketch.js
@@ -1,25 +1,26 @@
 import { circle } from "@thi.ng/geom";
+import { fit } from "@thi.ng/math";
 
 const settings = {
-  // animated: true,
-  // clearColor: "black",
+  animated: true,
+  clearColor: "black",
 };
 
 let circ;
 
 function update(time) {
-  console.log(time);
-  circ = circle([0, 0], 0.25);
+  let rad = fit(Math.sin(time / 1000), -1, 1, 0.2, 0.8);
+  circ = circle([0, 0], rad);
 }
 
 function render({ ctx, canvasScale }) {
   ctx.beginPath();
   let [x, y] = circ.pos;
   ctx.arc(x, x, circ.r, 0, 2 * Math.PI, false);
-  ctx.fillStyle = "green";
+  // ctx.fillStyle = "white";
   ctx.fill();
   ctx.strokeWidth(9);
-  ctx.strokeStyle = "#003300";
+  ctx.strokeStyle = "#fff";
   ctx.stroke();
 }
 
